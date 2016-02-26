5598e61b536391d563b31dc583aca96fb832f5c8
HopeAsd <qfeizaijun@qq.com>
2016-02-27 00:22:27 +0800

    update version 1.0.3
    修复一些已知BUG 添加了BILI按钮功能

--- src/main/java/io/bilicraft/r6/clientui/BilicraftUI.java
+++ src/main/java/io/bilicraft/r6/clientui/BilicraftUI.java
@@ -2,34 +2,58 @@ package io.bilicraft.r6.clientui;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import io.bilicraft.r6.clientui.handler.BilicraftDebugHandler;
+import io.bilicraft.r6.clientui.handler.BilicraftMenuHandler;
+import io.bilicraft.r6.clientui.handler.BilicraftPlayerHandler;
+import io.bilicraft.r6.clientui.proxy.BilicraftCommonProxy;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.common.MinecraftForge;
 
-@Mod(modid = "bilicraftui",name="Bilicraft UI",version = "alpha")
+@Mod(modid = BilicraftUI.MODID, name = BilicraftUI.MODNAME, version = BilicraftUI.VERSION)
 public class BilicraftUI {
+
+	public static final String MODID = "bilicraftui";
+	public static final String MODNAME = "Bilicraft UI";
+	public static final String VERSION = "v1.0.3";
+	private static final boolean Debug = false;
+
 	public static String email;
 	public static String password;
 	@Mod.Instance
 	public static BilicraftUI instance;
-	private static final boolean Debug = true;
+
+	@SidedProxy(clientSide = "io.bilicraft.r6.clientui.proxy.BilicraftClientProxy", serverSide = "io.bilicraft.r6.clientui.proxy.BilicraftCommonProxy", modId = MODID)
+	public static BilicraftCommonProxy proxy;
+	private static boolean showPlayerModel = false;
 	@EventHandler
-	public void onModPreInit(FMLPreInitializationEvent event)
-	{
+	public void onModPreInit(FMLPreInitializationEvent event) {
 		MinecraftForge.EVENT_BUS.register(new BilicraftMenuHandler());
 		MinecraftForge.EVENT_BUS.register(new BilicraftPlayerHandler());
+		
 		if (isDebug()) {
 			MinecraftForge.EVENT_BUS.register(new BilicraftDebugHandler());
 		}
-		
 	}
-	public static boolean isDebug()
+	@EventHandler
+	public void onModInit(FMLInitializationEvent event)
 	{
+		proxy.oninit(event);
+	}
+	public static boolean isDebug() {
 		return Debug;
 	}
-	public static void sendCommandExecute(String command)
+	public static boolean isShowPlayer()
 	{
+		return showPlayerModel;
+	}
+	public static void setPlayerModelState()
+	{
+		showPlayerModel = !isShowPlayer();
+	}
+	public static void sendCommandExecute(String command) {
 		Minecraft.getMinecraft().thePlayer.sendChatMessage(command);
 	}
 }
-
---
+++ src/main/java/io/bilicraft/r6/clientui/handler/BilicraftMenuHandler.java
@@ -0,0 +1,29 @@
+package io.bilicraft.r6.clientui.handler;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import io.bilicraft.r6.clientui.BilicraftUI;
+import io.bilicraft.r6.clientui.render.BilicraftMainMenu;
+import net.minecraft.client.gui.GuiMainMenu;
+import net.minecraft.client.multiplayer.GuiConnecting;
+import net.minecraftforge.client.event.GuiOpenEvent;
+
+public class BilicraftMenuHandler {
+	public static BilicraftMenuHandler instance = new BilicraftMenuHandler();
+	public static Logger logging = LogManager.getLogger("MenuHandler");
+
+	@SubscribeEvent
+	public void onMenuOpen(GuiOpenEvent event) {
+		if (event.gui instanceof GuiMainMenu) {
+			event.gui = new BilicraftMainMenu();
+			
+		}
+		if (event.gui instanceof GuiConnecting) {
+			if (BilicraftUI.isDebug()) {
+				logging.info(BilicraftUI.email + BilicraftUI.password);
+			}
+		}
+	}
+}
