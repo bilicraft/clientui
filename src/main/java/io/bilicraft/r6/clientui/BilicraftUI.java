fc18b5f64ce0ed721922a16cb8f7d3c01e0282cb
HopeAsd <qfeizaijun@qq.com>
2016-02-27 01:01:35 +0800

    Revert "update version 1.0.3"
    This reverts commit 5598e61b536391d563b31dc583aca96fb832f5c8.

--- src/main/java/io/bilicraft/r6/clientui/BilicraftUI.java
+++ src/main/java/io/bilicraft/r6/clientui/BilicraftUI.java
@@ -2,58 +2,34 @@ package io.bilicraft.r6.clientui;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.SidedProxy;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import io.bilicraft.r6.clientui.handler.BilicraftDebugHandler;
-import io.bilicraft.r6.clientui.handler.BilicraftMenuHandler;
-import io.bilicraft.r6.clientui.handler.BilicraftPlayerHandler;
-import io.bilicraft.r6.clientui.proxy.BilicraftCommonProxy;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.common.MinecraftForge;
 
-@Mod(modid = BilicraftUI.MODID, name = BilicraftUI.MODNAME, version = BilicraftUI.VERSION)
+@Mod(modid = "bilicraftui",name="Bilicraft UI",version = "alpha")
 public class BilicraftUI {
-
-	public static final String MODID = "bilicraftui";
-	public static final String MODNAME = "Bilicraft UI";
-	public static final String VERSION = "v1.0.3";
-	private static final boolean Debug = false;
-
 	public static String email;
 	public static String password;
 	@Mod.Instance
 	public static BilicraftUI instance;
-
-	@SidedProxy(clientSide = "io.bilicraft.r6.clientui.proxy.BilicraftClientProxy", serverSide = "io.bilicraft.r6.clientui.proxy.BilicraftCommonProxy", modId = MODID)
-	public static BilicraftCommonProxy proxy;
-	private static boolean showPlayerModel = false;
+	private static final boolean Debug = true;
 	@EventHandler
-	public void onModPreInit(FMLPreInitializationEvent event) {
+	public void onModPreInit(FMLPreInitializationEvent event)
+	{
 		MinecraftForge.EVENT_BUS.register(new BilicraftMenuHandler());
 		MinecraftForge.EVENT_BUS.register(new BilicraftPlayerHandler());
-		
 		if (isDebug()) {
 			MinecraftForge.EVENT_BUS.register(new BilicraftDebugHandler());
 		}
+		
 	}
-	@EventHandler
-	public void onModInit(FMLInitializationEvent event)
+	public static boolean isDebug()
 	{
-		proxy.oninit(event);
-	}
-	public static boolean isDebug() {
 		return Debug;
 	}
-	public static boolean isShowPlayer()
+	public static void sendCommandExecute(String command)
 	{
-		return showPlayerModel;
-	}
-	public static void setPlayerModelState()
-	{
-		showPlayerModel = !isShowPlayer();
-	}
-	public static void sendCommandExecute(String command) {
 		Minecraft.getMinecraft().thePlayer.sendChatMessage(command);
 	}
 }
+
